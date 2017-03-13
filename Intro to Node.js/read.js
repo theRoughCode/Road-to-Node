2d02fef058c44fa10fe88e63368c3fd739307e8a
raphael <raph.koh@gmail.com>
2017-03-12 23:40:00 -0400

    Update lesson

--- Intro to Node.js/read.js
+++ Intro to Node.js/read.js
@@ -4,7 +4,7 @@ var fs = require("fs");  // "imports" fs module
 // Takes in 2 parameters: name of file, encoding of file
 const text = fs.readFileSync('abc.txt', 'utf8');
 console.log(text);
-console.log("Done reading file!!\n");
+console.log("I am Synchronous!");
 
 // TO RUN THIS, COMMAND LINE -> "node test.js"
 
@@ -15,3 +15,19 @@ fs.readFile('abc.txt', 'utf8', function (err, data) {
   }
 });
 console.log("I am Asynchronous!");
+
+// Using a Promise
+// NOTE THAT PARAMS FOR PROMISE IS (fulfilled, rejected), REVERSE OF CALLBACKS
+function readFileAsync(file, encoding) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(file, encoding, function (err, data) {
+      if(err) return reject(err);
+      resolve(data);
+    })
+  })
+}
+
+readFileAsync('abc.txt', 'utf-8').then(data => console.log(data), err => console.error(err));
+
+
+console.log("I am also Asynchronous!");
